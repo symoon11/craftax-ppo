fcf3246617821872dce960f7f3c492a8c87904f4
symoon11 <symoon11@snu.ac.kr>
2025-02-20 21:07:08 +0900

    Change linen to nnx

--- utils/flax_util.py
+++ utils/flax_util.py
@@ -1,46 +1,48 @@
 from typing import Sequence
 
-import flax.linen as nn
 import jax.numpy as jnp
-from chex import Array
-from flax.linen.initializers import variance_scaling
+from flax import nnx
+from jax.typing import ArrayLike, DTypeLike
 
 
-class BaseLayer(nn.Module):
-    out_size: int
-    norm_type: str = "layer"
-    act_type: str = "relu"
-    scale: float = 1.0
-
-    def setup(self):
-        if self.norm_type == "none":
+class BaseLayer(nnx.Module):
+    def __init__(
+        self,
+        in_size: int,
+        *,
+        norm_type: str = "layer",
+        act_type: str = "relu",
+        scale: float = 1.0,
+        dtype: DTypeLike = jnp.bfloat16,
+        rngs: nnx.Rngs,
+    ):
+        if norm_type == "none":
             self.norm = None
-        elif self.norm_type == "layer":
-            self.norm = nn.LayerNorm(dtype=jnp.bfloat16)
+        elif norm_type == "layer":
+            self.norm = nnx.LayerNorm(in_size, dtype=dtype, rngs=rngs)
         else:
-            raise NotImplementedError(self.norm_type)
+            raise NotImplementedError(norm_type)
         use_bias = self.norm is None
-        kernel_init = variance_scaling(
-            self.scale,
-            mode="fan_in",
-            distribution="truncated_normal",
+        kernel_init = nnx.nn.initializers.variance_scaling(
+            scale, "fan_in", "truncated_normal", dtype=dtype
         )
         self.layer = None
         self.layer_kwargs = {
             "use_bias": use_bias,
             "kernel_init": kernel_init,
-            "dtype": jnp.bfloat16,
+            "dtype": dtype,
+            "rngs": rngs,
         }
-        if self.act_type == "none":
+        if act_type == "none":
             self.act = None
-        elif self.act_type == "silu":
-            self.act = nn.silu
-        elif self.act_type == "relu":
-            self.act = nn.relu
+        elif act_type == "silu":
+            self.act = nnx.silu
+        elif act_type == "relu":
+            self.act = nnx.relu
         else:
-            raise NotImplementedError(self.act_type)
+            raise NotImplementedError(act_type)
 
-    def __call__(self, x: Array) -> Array:
+    def __call__(self, x: ArrayLike) -> ArrayLike:
         if self.norm:
             x = self.norm(x)
         x = self.layer(x)
@@ -49,23 +51,57 @@ class BaseLayer(nn.Module):
         return x
 
 
-class Dense(BaseLayer):
-    def setup(self):
-        super().setup()
-        self.layer = nn.Dense(self.out_size, **self.layer_kwargs)
+class Linear(BaseLayer):
+    def __init__(
+        self,
+        in_size: int,
+        out_size: int,
+        *,
+        norm_type: str = "layer",
+        act_type: str = "relu",
+        scale: float = 1.0,
+        dtype: DTypeLike = jnp.bfloat16,
+        rngs: nnx.Rngs,
+    ):
+        super().__init__(
+            in_size,
+            norm_type=norm_type,
+            act_type=act_type,
+            scale=scale,
+            dtype=dtype,
+            rngs=rngs,
+        )
+        self.layer = nnx.Linear(in_size, out_size, **self.layer_kwargs)
 
 
 class Conv(BaseLayer):
-    kernel_size: Sequence[int] = (3, 3)
-    strides: Sequence[int] = (1, 1)
-    padding: str = "SAME"
-
-    def setup(self):
-        super().setup()
-        self.layer = nn.Conv(
-            self.out_size,
-            self.kernel_size,
-            strides=self.strides,
-            padding=self.padding,
+    def __init__(
+        self,
+        in_size: int,
+        out_size: int,
+        *,
+        kernel_size: Sequence[int] = (3, 3),
+        strides: Sequence[int] = (1, 1),
+        padding: str = "SAME",
+        norm_type: str = "layer",
+        act_type: str = "relu",
+        scale: float = 1.0,
+        dtype: DTypeLike = jnp.bfloat16,
+        rngs: nnx.Rngs,
+    ):
+        super().__init__(
+            in_size,
+            norm_type=norm_type,
+            act_type=act_type,
+            scale=scale,
+            dtype=dtype,
+            rngs=rngs,
+        )
+        self.layer = nnx.Conv(
+            in_size,
+            out_size,
+            kernel_size,
+            strides=strides,
+            padding=padding,
             **self.layer_kwargs,
         )
--- utils/impala_cnn.py
+++ utils/impala_cnn.py
@@ -1,98 +1,130 @@
+import math
 from typing import Sequence
 
-import flax.linen as nn
 import jax.numpy as jnp
-from chex import Array
+from flax import nnx
+from jax.typing import ArrayLike
 
-from utils.flax_util import Dense, Conv
+from utils.flax_util import Conv, Linear
 
 
-class BasicBlock(nn.Module):
-    out_chan: int
-    scale: float = 1.0
-
-    def setup(self):
-        super().setup()
-        scale = self.scale / jnp.sqrt(2)
+class BasicBlock(nnx.Module):
+    def __init__(
+        self, in_chan: int, out_chan: int, *, scale: float = 1.0, rngs: nnx.Rngs
+    ):
+        scale = scale / math.sqrt(2)
         self.conv1 = Conv(
-            self.out_chan,
+            in_chan,
+            out_chan,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="SAME",
             norm_type="layer",
             act_type="relu",
             scale=scale,
-            kernel_size=(3, 3),
-            strides=(1, 1),
-            padding="SAME",
+            dtype=jnp.bfloat16,
+            rngs=rngs,
         )
         self.conv2 = Conv(
-            self.out_chan,
-            norm_type="layer",
-            act_type="none",
-            scale=scale,
+            in_chan,
+            out_chan,
             kernel_size=(3, 3),
             strides=(1, 1),
             padding="SAME",
+            norm_type="layer",
+            act_type="relu",
+            scale=scale,
+            dtype=jnp.bfloat16,
+            rngs=rngs,
         )
 
-    def __call__(self, x: Array) -> Array:
+    def __call__(self, x: ArrayLike) -> ArrayLike:
         x = x + self.conv2(self.conv1(x))
         return x
 
 
-class DownStack(nn.Module):
-    out_chan: int
-    num_blocks: int
-    scale: float = 1.0
-    first_conv_norm: bool = False
-
-    def setup(self):
-        super().setup()
+class DownStack(nnx.Module):
+    def __init__(
+        self,
+        in_chan: int,
+        out_chan: int,
+        num_blocks: int,
+        *,
+        scale: float = 1.0,
+        first_conv_norm: bool = False,
+        rngs: nnx.Rngs,
+    ):
+        self.in_chan = in_chan
+        self.out_chan = out_chan
         self.conv1 = Conv(
-            self.out_chan,
-            norm_type="layer" if self.first_conv_norm else "none",
-            act_type="relu",
-            scale=1.0,
+            in_chan,
+            out_chan,
             kernel_size=3,
             strides=1,
             padding="SAME",
+            norm_type="layer" if first_conv_norm else "none",
+            act_type="relu",
+            scale=1.0,
+            dtype=jnp.bfloat16,
+            rngs=rngs,
         )
-        self.norm = nn.LayerNorm(dtype=jnp.bfloat16)
-        scale = self.scale / jnp.sqrt(self.num_blocks)
+        self.norm = nnx.LayerNorm(out_chan, dtype=jnp.bfloat16, rngs=rngs)
+        scale = scale / math.sqrt(num_blocks)
         self.blocks = [
-            BasicBlock(self.out_chan, scale=scale) for _ in range(self.num_blocks)
+            BasicBlock(out_chan, out_chan, scale=scale, rngs=rngs)
+            for _ in range(num_blocks)
         ]
 
-    def __call__(self, x: Array) -> Array:
+    def __call__(self, x: ArrayLike) -> ArrayLike:
         x = self.conv1(x)
-        x = nn.max_pool(x, window_shape=(3, 3), strides=(2, 2), padding="SAME")
+        x = nnx.max_pool(x, window_shape=(3, 3), strides=(2, 2), padding="SAME")
         x = self.norm(x)
         for block in self.blocks:
             x = block(x)
         return x
 
+    def output_shape(self, in_shape: Sequence[int]) -> Sequence[int]:
+        h, w, c = in_shape
+        assert c == self.in_chan
+        return ((h + 1) // 2, (w + 1) // 2, self.out_chan)
 
-class ImpalaCNN(nn.Module):
-    out_chans: Sequence[int]
-    num_blocks: int
-    out_size: int
-    first_conv_norm: bool = False
 
-    def setup(self):
-        super().setup()
-        scale = 1.0 / jnp.sqrt(len(self.out_chans))
-        self.stacks = [
-            DownStack(
+class ImpalaCNN(nnx.Module):
+    def __init__(
+        self,
+        in_shape: Sequence[int],
+        out_chans: Sequence[int],
+        num_blocks: int,
+        out_size: int,
+        *,
+        first_conv_norm: bool = False,
+        rngs: nnx.Rngs,
+    ):
+        scale = 1.0 / math.sqrt(len(out_chans))
+        self.stacks = []
+        for i, out_chan in enumerate(out_chans):
+            stack = DownStack(
+                in_shape[-1],
                 out_chan,
-                self.num_blocks,
+                num_blocks,
                 scale=scale,
-                first_conv_norm=self.first_conv_norm if i == 0 else True,
+                first_conv_norm=first_conv_norm if i == 0 else True,
+                rngs=rngs,
             )
-            for i, out_chan in enumerate(self.out_chans)
-        ]
-        self.dense = Dense(
-            self.out_size, norm_type="layer", act_type="relu", scale=jnp.sqrt(2)
+            self.stacks.append(stack)
+            in_shape = stack.output_shape(in_shape)
+        in_size = math.prod(in_shape)
+        self.dense = Linear(
+            in_size,
+            out_size,
+            norm_type="layer",
+            act_type="relu",
+            scale=math.sqrt(2),
+            dtype=jnp.bfloat16,
+            rngs=rngs,
         )
 
-    def __call__(self, x: Array) -> Array:
+    def __call__(self, x: ArrayLike) -> ArrayLike:
         for stack in self.stacks:
             x = stack(x)
         x = jnp.reshape(x, (*x.shape[:2], -1))
